644da44f3532cf4e84af1fcee327c026b1e6abda
tdrayson <taylor@drayson.co.uk>
2026-03-05 11:07:28 +0000

    Update Vite configuration and asset paths for CinemaSync
    - Set base path in vite.config.js to '/cinema-sync/' for proper asset loading.
    - Updated index.html to reference new asset paths for JavaScript and CSS files.
    - Added new JavaScript and CSS files to enhance application functionality and styling.
    - Removed outdated CSS file to streamline the project.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/cinema-sync/',
   build: { outDir: 'docs', assetsInlineLimit: 0 },
   plugins: [vue(), tailwindcss()],
 });
